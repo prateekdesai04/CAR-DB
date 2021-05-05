548123897c8458ed83302f50dc6d0f9f0384214c
PrateekMDesai04 <prateekmdesai04@gmail.com>
2021-05-05 15:12:45 +0400

    DB Connection

--- server/app.js
+++ server/app.js
@@ -2,14 +2,24 @@ const express = require('express');
 const app = express();
 const { graphqlHTTP } = require('express-graphql'); //allows express to understand GraphQL
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+require('dotenv/config'); // to get .env variables
 
 app.use(
+  //This function handles the graphql requests - middleware
   '/graphql',
   graphqlHTTP({
     schema: schema,
     graphiql: true,
   })
-); //This function handles the graphql requests - middleware
+);
+
+// CONNECT TO DB
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  () => console.log('Connected to DB !')
+);
 
 app.listen(4200, () => {
   console.log('Server up and running...');
